6008e555eca3d3efd75ec9a1bb3400af4356a27c
William A Rodriguez <wrod@mit.edu>
2020-03-15 19:25:00 -0400

    First commit

---
+++ functions/index.js
@@ -0,0 +1,144 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+const functions = require('firebase-functions');
+// const serviceAccount = require('./icommute-firebase-firebase-adminsdk-k6k3e-ede02571bf.json')
+const apiKeys = require('./apiKeys.json');
+const fetch = require('node-fetch');
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+// admin.initializeApp();
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   databaseURL: 'https://icommute-firebase.firebaseio.com/'
+// });
+
+
+admin.initializeApp({
+   credential: admin.credential.applicationDefault()
+ });
+
+
+const DISTANCES_API_URL = 'https://maps.googleapis.com/maps/api/distancematrix/json?'
+
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello from Firebase @_@");
+});
+
+
+
+function getDistance(from,to) {
+  var requestUrl = DISTANCES_API_URL;
+  requestUrl+=`key=${apiKeys.googleplaces}&origins=${from}&destinations=${to}`
+
+  return request(requestUrl, function (error, response, body) {
+    object = JSON.parse(body);
+    const firstElement = object["rows"][0]["elements"][0];
+    if (firstElement["status"] === "OK"){
+      const distanceText = firstElement["distance"]["text"];
+      const distanceValue = firstElement["distance"]["value"];
+      const durationText = firstElement["duration"]["text"];
+      const durationValue = firstElement["duration"]["value"]
+      return response.json({distanceText:distanceText,durationText:durationText});
+
+    } else{
+      return response.json({distanceText:"REQUEST STATUS NOT OK",durationText:"REQUEST STATUS NOT OK"});
+    }
+  });
+}
+
+
+function scheduleCommutes(){
+  const ref = admin.database().ref('/users/test_user/commutes');
+  return ref.once('value').then((snapShotCommutes) => {
+      // work with the snapshot here, and return another promise
+      // that resolves after all your updates are complete
+          const commutesJSON = snapShotCommutes.toJSON();
+          const timstampHashes = Object.keys(commutesJSON);
+
+          // var computedCommutes = [];
+          //
+          // timstampHashes.forEach((hash, i) => {
+          //     commuteObject = commutesJSON[hash];
+          //     const from = commuteObject["from"];
+          //     const to = commuteObject["to"];
+          //
+          //     transitInfo = getDistance(from,to);
+          //     computedCommutes.push(transitInfo);
+          //
+          // });
+          return timstampHashes;
+
+      });
+    }
+
+
+
+exports.updateCommutes = functions.https.onRequest((request, response) => {
+  console.log("Called updateCommutes latest");
+  const ref = admin.database().ref('/users/test_user/commutes');
+
+  ref.once('value').then((snapshot)=>{
+    response.send(snapshot);
+  })
+  .catch((error)=>{
+    response.send("error@: "+error)
+  })
+
+});
+
+
+
+
+
+exports.getDistance = functions.https.onRequest((req, res) => {
+  const from = req.query.from;
+  const to = req.query.to;
+
+  var requestUrl = DISTANCES_API_URL;
+
+
+  requestUrl+=`key=${apiKeys.googleplaces}&origins=${from}&destinations=${to}`
+
+
+  fetch(requestUrl)
+  .then(response => response.json())
+  .then(data => {
+    return res.json(data);
+
+    // const firstElement = object["rows"][0]["elements"][0];
+    // const status = firstElement["status"];
+    // if (status === "OK"){
+    //   const distanceText = firstElement["distance"]["text"];
+    //   const distanceValue = firstElement["distance"]["value"];
+    //   const durationText = firstElement["duration"]["text"];
+    //   const durationValue = firstElement["duration"]["value"];
+    //   return res.json({distanceText:distanceText,durationText:durationText});
+    //
+    // } else{
+    //   return res.json({distanceText:"REQUEST STATUS NOT OK",durationText:"REQUEST STATUS NOT OK"});
+    // }
+
+  })
+  .catch(err => "error@:"+err)
+});
+
+
+
+//   return request(requestUrl, function (error, response, body) {
+//     object = JSON.parse(body);
+//     const firstElement = object["rows"][0]["elements"][0];
+//     if (firstElement["status"] === "OK"){
+//       const distanceText = firstElement["distance"]["text"];
+//       const distanceValue = firstElement["distance"]["value"];
+//       const durationText = firstElement["duration"]["text"];
+//       const durationValue = firstElement["duration"]["value"]
+//       return res.json({distanceText:distanceText,durationText:durationText});
+//
+//     } else{
+//       return res.json({distanceText:"REQUEST STATUS NOT OK",durationText:"REQUEST STATUS NOT OK"});
+//     }
+//   });
+// });
